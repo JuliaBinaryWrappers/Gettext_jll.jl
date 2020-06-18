f21ea1c5b0a1621d735a82222c30f8051bfdc44d
jlbuild <juliabuildbot@gmail.com>
2020-06-18 19:34:54 +0000

    Gettext_jll build 0.20.1+3

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -7,6 +7,7 @@ using XML2_jll
 PATH = ""
 LIBPATH = ""
 LIBPATH_env = "PATH"
+LIBPATH_default = ""
 
 # Relative path to `libgettext`
 const libgettext_splitpath = ["bin", "libgettextlib-0-20-1.dll"]
@@ -30,13 +31,13 @@ function __init__()
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
     # then append them to our own.
     foreach(p -> append!(PATH_list, p), (Libiconv_jll.PATH_list, XML2_jll.PATH_list,))
     foreach(p -> append!(LIBPATH_list, p), (Libiconv_jll.LIBPATH_list, XML2_jll.LIBPATH_list,))
 
+    # Lastly, we need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     global libgettext_path = normpath(joinpath(artifact_dir, libgettext_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
